69820c8279e5f55db95689f6d8962291fc7a51b6
Vargas Casaseca, Cesar <cesar.vargas-casaseca@asideas.de>
2015-03-17 11:35:46 +0100

    Fix if index is same as current.

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -1418,6 +1418,13 @@
 }
 
 - (void)setCurrentPhotoIndex:(NSUInteger)index animated:(BOOL)animated completion:(void (^)())completion; {
+  if (index == _currentPageIndex) {
+    if (completion) {
+      completion();
+      return;
+    }
+  }
+  
     // Validate
     NSUInteger photoCount = [self numberOfPhotos];
     if (photoCount == 0) {
